0e5e7fc53bbbd8de9a1c36878842a54cde3ec2eb
kcalvinroy <kcalvinroy@gmail.com>
2025-02-27 01:56:07 +0300

    Bounded fix

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,17 +1,17 @@
 import clsx from "clsx";
 
-type BoundedProps = {
-  as?: React.ElementType;
+type BoundedProps<T extends React.ElementType> = {
+  as?: T;
   className?: string;
   children: React.ReactNode;
-};
+} & React.ComponentPropsWithRef<T>;
 
-export const Bounded = ({
+export const Bounded = <T extends React.ElementType>({
   as: Comp = "section",
   className,
   children,
   ...restProps
-}: BoundedProps) => {
+}: BoundedProps<T>) => {
   return (
     <Comp
       className={clsx("px-4 first:pt-10 md:px-6", className)}
