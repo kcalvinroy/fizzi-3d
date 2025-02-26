76294e2fdb32eada5d2e62011f514e940184b27c
kcalvinroy <kcalvinroy@gmail.com>
2025-02-26 22:33:39 +0300

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { FizziLogo } from "./FizziLogo";
+
+type Props = {};
+
+export default function Header({}: Props) {
+  return (
+    <header className="-mb-28 flex justify-center py-4">
+      <FizziLogo className="z-10 h-20 cursor-pointer text-sky-800" />
+    </header>
+  );
+}
