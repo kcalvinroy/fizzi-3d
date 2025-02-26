2c35376ce0344003d3e5256a3ae96abe5e8e6114
kcalvinroy <kcalvinroy@gmail.com>
2025-02-26 22:46:37 +0300

    Eslint rule update commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,19 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/ban-types": [
+        "error",
+        {
+          types: {
+            "{}": false, // Allow empty object type
+          },
+          extendDefaults: true,
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
